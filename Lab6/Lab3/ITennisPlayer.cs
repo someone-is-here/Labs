b27c4921899156131a2586b5acce8d59bd84ac24
Tanusha <48770804+someone-is-here@users.noreply.github.com>
2021-04-14 22:45:23 +0300

    Update ITennisPlayer.cs

--- Lab6/Lab3/ITennisPlayer.cs
+++ Lab6/Lab3/ITennisPlayer.cs
@@ -2,9 +2,9 @@
 
 namespace Lab3 {
     public interface ITennisPlayer {
-        public int RankingPosition { get; set; }
-        public bool IsWimbledonWinner { get; set; }
-        public double HitRate { get; set; }
-        public void GenTennisPlayer();
+        int RankingPosition { get; set; }
+        bool IsWimbledonWinner { get; set; }
+        double HitRate { get; set; }
+        void GenTennisPlayer();
     }
 }
