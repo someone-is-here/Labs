307cd04c38f7cee0e25e01710fe9974da2f67908
Tanusha <48770804+someone-is-here@users.noreply.github.com>
2021-12-05 19:30:40 +0300

    Add files via upload

---
+++ Lab11_053501_Shurko/ClassLibrary/Container.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary {
+    public class Container {
+        public Container(double start, double end) {
+            if (start < end) {
+                this.Start = start;
+                this.End = end;
+            }
+            else {
+                throw new Exception("Wrong paramentrs!(start > end)");
+            }
+        }
+        public readonly double Start;
+        public readonly double End;
+    }
+}
---
+++ Lab11_053501_Shurko/ClassLibrary/Integral.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ClassLibrary {
+    public class Integral {
+        public delegate void HandlerForCalc(TimeSpan ts, double res,string name);
+        public static event HandlerForCalc Calculation;
+
+        private static readonly double step = 0.0000001;
+        public static void CountIntegral(object obj) {
+            Console.WriteLine("CountIntegral");
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Container container = (Container)obj;
+            double start = container.Start;
+            double end = container.End;
+            Stopwatch stopWatch = new();
+            stopWatch.Start();
+            double sum = 0;
+            for (double i = start; i < end; i += step) {
+                sum += (i * Math.Sin(i));
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Calculation?.Invoke(ts, sum, Thread.CurrentThread.Name);
+        }
+        public static async Task CountIntegralAsync(object obj) {
+            await Task.Run(() => {
+                Console.WriteLine("CountIntegralAsync");
+                CountIntegral(obj);
+            });
+        }
+    }
+}
